1e9da165c490089d381824b2a236aa3d9731708b
Luis Bastidas <luisbastir@gmail.com>
2018-09-05 21:14:00 -0700

    Service app added

--- django2webcompany/urls.py
+++ django2webcompany/urls.py
@@ -15,10 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 
 urlpatterns = [
-    # Core urls
-    path('', include('core.urls')),
-    # Admin urls
-    path('admin/', admin.site.urls),
+  # Core urls
+  path('', include('core.urls')),
+  # Services urls
+  path('services/', include('services.urls')),
+  # Admin urls
+  path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+  from django.conf.urls.static import static
+  urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
